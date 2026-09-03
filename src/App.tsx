9446295053baa225c771f11f934194d5a6920d71
santimat <santimaturo1@gmail.com>
2026-09-03 19:53:27 -0300

    add a test page to try stuffs

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router";
 import { AuthPage } from "@/pages/Auth";
+import { Test } from "@/pages/Test";
 
 function App() {
-	return (
-		<Routes>
-			<Route path="/auth" element={<AuthPage />} />
-		</Routes>
-	);
+  return (
+    <Routes>
+      <Route path="/auth" element={<AuthPage />} />
+      <Route path="/test" element={<Test />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/pages/Test.tsx
@@ -0,0 +1,3 @@
+export function Test() {
+  return <h1 className="text-red-700 text-9xl">Hola!</h1>;
+}
