5153f35c1a3c972007a55df1014c74e0e2b283a6
santimat <santimaturo1@gmail.com>
2026-09-10 14:18:33 -0300

    fix some paddings

--- src/shared/components/SideBar.tsx
+++ src/shared/components/SideBar.tsx
@@ -1,49 +1,44 @@
-// Acá tenes importa el Icono de la tiendita
-// el "@" se usa como un alias para la ruta ./src/
 import { StoreIcon } from "@/shared/icons/StoreIcon";
-import { PeopleIcon } from "../icons/PeopleIcon";
-import { DashboardIcon } from "../icons/DashboardIcon";
-import { BarcodeIcon } from "../icons/BarcodeIcon";
-import { BoxIcon } from "../icons/BoxIcon";
-import { DesignIcon } from "../icons/DesignIcon";
-import { ExitIcon } from "../icons/ExitIcon";
-import { UserIcon } from "../icons/UserIcon";
+import { DashboardIcon } from "@/shared/icons/DashboardIcon";
+import { BarcodeIcon } from "@/shared/icons/BarcodeIcon";
+import { BoxIcon } from "@/shared/icons/BoxIcon";
+import { DesignIcon } from "@/shared/icons/DesignIcon";
+import { ExitIcon } from "@/shared/icons/ExitIcon";
+import { UserIcon } from "@/shared/icons/UserIcon";
+import { PeopleIcon } from "@/shared/icons/PeopleIcon";
 
 export function SideBar() {
 	return (
-		<aside className="bg-background w-64 h-screen py-4 gap-4 border-r border-background-soft flex-col flex items-start justify-items-start ">
-
-			<header className="flex items-center gap-3 w-full px-4 pb-6 border-b border-background-soft ">
-				<div className="bg-primary text-white p-2 rounded-md flex items-center justify-center" >
-						<StoreIcon className="w-5 h-5" />
-				</div>
-				<p className="font-semibold">GestiON</p>
+		<aside className="bg-background max-w-64 h-screen gap-4 border-r border-background-soft flex-col flex items-start justify-items-start">
+			<header className="flex items-center gap-3 w-full p-4 border-b border-background-soft ">
+				<StoreIcon className="w-8 h-8 text-white p-1 bg-primary rounded-md" />
+				<h1 className="font-semibold text-xl">GestiON</h1>
 			</header>
 
-			<main className="w-full pt-4 px-6 text-gray-600">
+			<main className="w-full p-4 text-gray-600">
 				<ul className="flex flex-col gap-4">
 					<li className="group flex items-center gap-4 cursor-pointer -mx-2 px-2 py-2 rounded-lg hover:bg-gray-200 transition-colors duration-150">
-						<DashboardIcon className="group-hover:text-sky-400 transition-colors" />
+						<DashboardIcon className="group-hover:text-sky-400 transition-colors w-6 h-auto" />
 						<p className="group-hover:text-gray-900">Dashboard</p>
 					</li>
 
 					<li className="group flex items-center gap-4 cursor-pointer -mx-2 px-2 py-2 rounded-lg hover:bg-gray-200 transition-colors duration-150">
-						<BarcodeIcon className="group-hover:text-sky-400 transition-colors"/>
+						<BarcodeIcon className="group-hover:text-sky-400 transition-colors w-6 h-auto" />
 						<p className="group-hover:text-gray-900">Puntos de venta</p>
 					</li>
 
 					<li className="group flex items-center gap-4 cursor-pointer -mx-2 px-2 py-2 rounded-lg hover:bg-gray-200 transition-colors duration-150">
-						<BoxIcon className="group-hover:text-sky-400 transition-colors"/>
+						<BoxIcon className="group-hover:text-sky-400 transition-colors w-6 h-auto" />
 						<p className="group-hover:text-gray-900">Productos</p>
 					</li>
 
 					<li className="group flex items-center gap-4 cursor-pointer -mx-2 px-2 py-2 rounded-lg hover:bg-gray-200 transition-colors duration-150">
-						<PeopleIcon className="group-hover:text-sky-400 transition-colors"/>
+						<PeopleIcon className="group-hover:text-sky-400 transition-colors w-6 h-auto" />
 						<p className="group-hover:text-gray-900">Cajeros</p>
 					</li>
 
 					<li className="group flex items-center gap-4 cursor-pointer -mx-2 px-2 py-2 rounded-lg hover:bg-gray-200 transition-colors duration-150">
-						<DesignIcon className="group-hover:text-sky-400 transition-colors"/>
+						<DesignIcon className="group-hover:text-sky-400 transition-colors w-6 h-auto" />
 						<p className="group-hover:text-gray-900">Sistema de diseño</p>
 					</li>
 				</ul>
@@ -60,5 +55,3 @@ export function SideBar() {
 		</aside>
 	);
 }
-
-
