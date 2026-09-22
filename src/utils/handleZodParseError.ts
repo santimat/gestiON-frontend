b95b6579873f7e6d4853ea45130e0456c431708e
santimat <santimaturo1@gmail.com>
2026-09-22 17:04:29 -0300

    start with commerceService

--- src/utils/handleZodParseError.ts
+++ src/utils/handleZodParseError.ts
@@ -1,25 +1,25 @@
 import type { AppError, FieldErrors, ZodParseResult } from "@/types";
 
-export function handleZodParsingError(parsedLogin: ZodParseResult) {
-	if (!parsedLogin.success) {
-		const { issues } = parsedLogin.error;
+export function handleZodParsingError(parsedData: ZodParseResult) {
+  if (!parsedData.success) {
+    const { issues } = parsedData.error;
 
-		const parsedErrors = // reduce nos permite usar una variable acumuladora la cual luego retornamos, en este caso un objeto
-			issues.reduce<FieldErrors>(
-				(acc, { path, message }) => {
-					const field = path[0] as keyof FieldErrors;
-					// usamos cada campo como clave y le asignamos el mensaje correspondiente
-					acc[field] = message;
-					return acc;
-				},
-				// valor por default de la variable acc
-				{},
-			);
+    const parsedErrors = // reduce nos permite usar una variable acumuladora la cual luego retornamos, en este caso un objeto
+      issues.reduce<FieldErrors>(
+        (acc, { path, message }) => {
+          const field = path[0] as keyof FieldErrors;
+          // usamos cada campo como clave y le asignamos el mensaje correspondiente
+          acc[field] = message;
+          return acc;
+        },
+        // valor por default de la variable acc
+        {},
+      );
 
-		throw {
-			type: "VALIDATION_ERROR",
-			message: "Error de validación.",
-			fieldErrors: parsedErrors,
-		} satisfies AppError;
-	}
+    throw {
+      type: "VALIDATION_ERROR",
+      message: "Error de validación.",
+      fieldErrors: parsedErrors,
+    } satisfies AppError;
+  }
 }
